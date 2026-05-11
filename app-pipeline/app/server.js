e4a5bac61d5e5692c810ed0529e8e31f3d77beb7
poisollan <mohankumar46456@gmail.com>
2026-05-11 18:53:57 +0530

    latest

--- app-pipeline/app/server.js
+++ app-pipeline/app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>App deployed by AWS CodePipeline to EKS</h1>");
+  res.send("<h1>Hi This is Mohan</h1>");
 });
 
 app.listen(3000, () => {
