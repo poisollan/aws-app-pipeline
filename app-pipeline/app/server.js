ec8a19494f1ab28317c3801eb69f3a3730b12fb6
poisollan <mohankumar46456@gmail.com>
2026-05-11 15:24:30 +0530

    Initial app pipeline setup

---
+++ app-pipeline/app/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>App deployed by AWS CodePipeline to EKS</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+});
